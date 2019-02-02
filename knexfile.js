76f8fbc00b4753d1292beb5a788ba863872f1679
amypetrie <apetrie9849@gmail.com>
2019-02-02 11:29:35 -0700

    Add production connection additional info to knex.js

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,10 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://localhost/quantified_test',
+    connection: {
+      database: 'quantified_self',
+      postgres: 'ujmfurijbrelns:9bf8bc8483cc90d2326976c1391e143af7b0ccc263f2ee880613d8998fa56f17@ec2-23-21-171-25.compute-1.amazonaws.com:5432/df4gn4tr2jthtv'
+    },
     migrations: {
       directory: './db/migrations'
     },
